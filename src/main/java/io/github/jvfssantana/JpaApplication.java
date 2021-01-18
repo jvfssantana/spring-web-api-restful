1fcf4e18817d1f06329ae38ec10c01580e047d2d
João <joaovitor277@yahoo.com.br>
2021-01-18 16:32:46 -0300

    Implementação do método (verbo http) GET na classe ClienteController

--- src/main/java/io/github/jvfssantana/JpaApplication.java
+++ src/main/java/io/github/jvfssantana/JpaApplication.java
@@ -1,8 +1,5 @@
 package io.github.jvfssantana;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,38 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import io.github.jvfssantana.entity.Cliente;
-import io.github.jvfssantana.entity.Pedido;
 import io.github.jvfssantana.repository.Clientes;
-import io.github.jvfssantana.repository.Pedidos;
 
 @SpringBootApplication
 public class JpaApplication {
-	
+
 	@Bean
-	public CommandLineRunner init(@Autowired Clientes clientes, @Autowired Pedidos pedidos) {
+	public CommandLineRunner init(@Autowired Clientes clientes) {
 		return args -> {
-			
-			System.out.println("Salvando clientes!");
-			
 			Cliente cliente = new Cliente("João");
 			clientes.save(cliente);
-			
-			Pedido pedido = new Pedido();
-			pedido.setCliente(cliente);
-			pedido.setData_pedido(LocalDate.now());
-			pedido.setTotal(BigDecimal.valueOf(10));
-			
-			pedidos.save(pedido);
-			
-			
-//			Cliente clientePedidos = clientes.findClienteFetchPedidos(cliente.getId());
-//			System.out.println(clientePedidos);
-//			System.out.println(clientePedidos.getPedidos());
-			
-			pedidos.findByCliente(cliente).forEach(System.out::println);
 		};
 	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
