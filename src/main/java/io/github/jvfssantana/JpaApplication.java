52c156d1fecb23db03fc7e17d75de6e3c2745bbd
João <joaovitor277@yahoo.com.br>
2021-01-18 20:34:42 -0300

    Conclusão dos métodos http para cadastro de cliente

--- src/main/java/io/github/jvfssantana/JpaApplication.java
+++ src/main/java/io/github/jvfssantana/JpaApplication.java
@@ -1,25 +1,11 @@
 package io.github.jvfssantana;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.jvfssantana.entity.Cliente;
-import io.github.jvfssantana.repository.Clientes;
 
 @SpringBootApplication
 public class JpaApplication {
 
-	@Bean
-	public CommandLineRunner init(@Autowired Clientes clientes) {
-		return args -> {
-			Cliente cliente = new Cliente("João");
-			clientes.save(cliente);
-		};
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
