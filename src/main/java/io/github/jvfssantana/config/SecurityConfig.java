0f0488548bbeb267b44435f5f71327b1cac6d235
João Vitor Felício Santana <joao.santana@senior.com.br>
2021-02-16 22:01:44 -0300

    Implementando a autenticação basic, userDetailService e a entidade Usuario como suas classes de serviço e controllers

--- src/main/java/io/github/jvfssantana/config/SecurityConfig.java
+++ src/main/java/io/github/jvfssantana/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.github.jvfssantana.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,8 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import io.github.jvfssantana.service.implementacao.UsuarioServiceImplementacao;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UsuarioServiceImplementacao usuarioService;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -18,16 +24,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.passwordEncoder(passwordEncoder())
-			.withUser("João")
-			.password(passwordEncoder().encode("123"))
-			.roles("USER");
+		auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/api/clientes/**")
+					.hasAnyRole("USER", "ADMIN")
+					.antMatchers("/api/pedidos/**")
+					.hasAnyRole("USER", "ADMIN")
+				.antMatchers("/api/produtos/**")
+					.hasRole("ADMIN")
+			.and().httpBasic();
 	}
 	
 }
